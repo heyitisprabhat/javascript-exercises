96181da716495b69532d153bba6aa690fc42ec09
Prabhat Rai <heyitisprabhat@gmail.com>
2026-02-12 21:26:59 +0530

    create removeFromArray function using arr.filter()

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function( arr, ...valuesToExclude) {
+    return arr.filter( item => !valuesToExclude.includes(item))
 };
+
+removeFromArray([1,2,3,4,5,6,7,8,9,0], 5,6,7)
 
 // Do not edit below this line
 module.exports = removeFromArray;
