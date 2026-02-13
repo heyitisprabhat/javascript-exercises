baba33529de414c11433e88081b79c5541543ed5
Prabhat Rai <heyitisprabhat@gmail.com>
2026-02-13 13:13:01 +0530

    create function sumAll using for loop and if/else

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (initial, final) {
+  let sum = 0;
+  if(!Number.isInteger(initial) || !Number.isInteger(final) || initial < 0 || final < 0){
+    return 'ERROR';
+  }
+  else if (initial <= final) {
+    for (let i = initial; i <= final; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = initial; i >= final; i--) {
+      sum += i;
+    }
+  }
 
+  return sum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
